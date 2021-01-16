bc73c15b265193f236ac50a96205c93db0e119cc
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-01-16 20:31:38 +0530

    pre increment post increment

--- app/src/main/java/com/example/kotlinbasic/kotlinprogramms/Operators.kt
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/Operators.kt
@@ -1,12 +1,65 @@
 package com.example.kotlinbasic.kotlinprogramms
 
 fun main() {
-    var a=13
-    var b=2
-    println(a>b) //true
-    println(a<b) //false
-    println(a>=b) // true
-    println(a==b) // false
-    println(a!=b) //true
+   var a=10
+   var b=10
+
+    /* a++
+    println(a)
+     //11*/
+
+    /* a = a++
+     println(a)
+     //10*/
+
+  /*  a =a++ + 5
+    println(a)
+    //output:15*/
+
+   /* ++a
+    println(a)
+    //11*/
+
+  /*  a = ++a
+    println(a)
+    // 11*/
+
+ /*   a = ++a +5
+    println(a)
+    // 16*/
+
+   /* a = a++ + b++
+    println(a)
+    // 20*/
+
+/*  a = a++ + b++
+    println(a)
+    println(b)
+    // 20
+    //11*/
+
+   /* a++ + b++
+    println(a)
+    println(b)
+    // 11
+    //11*/
+
+    /*   a = a++ + ++b
+    println(a)
+    // 21*/
+
+/*       a = a++ + ++b
+    println(a)
+     // 21 */
+
+/*    a =a++ + ++a
+    println(a)
+     //22*/
+
+  /*      a =++a + ++a
+    println(a)
+    //23*/
+
+
 
 }
