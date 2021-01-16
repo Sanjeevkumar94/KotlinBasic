1844d0be31b131f9813b372f1f09eb707cf84ad8
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-01-16 19:38:23 +0530

    type checking and type inference

---
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/Operators.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinbasic.kotlinprogramms
+
+fun main() {
+    var a=13
+    var b=2
+    println(a+b)
+    println(a-b)
+    println(a.toFloat()/b)
+    println(a.toFloat()*b)
+}
--- app/src/main/java/com/example/kotlinbasic/kotlinprogramms/Variables.kt
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/Variables.kt
@@ -2,7 +2,7 @@ package com.example.kotlinbasic.kotlinprogramms
 
 fun main() {
     val score = 5// now score is int type its called type inference
-    score=10.5  //error:
+//    score=10.5  //error:
                 // The floating-point literal does not conform to the expected type Int
                 // if you assign int value later you can not assign another data type of value called type checking.
 }
