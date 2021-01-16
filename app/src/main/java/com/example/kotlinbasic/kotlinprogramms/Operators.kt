5e86b5148d3cc8610cb91b708b38fdee9d5da43d
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-01-16 19:45:39 +0530

    relational operator

--- app/src/main/java/com/example/kotlinbasic/kotlinprogramms/Operators.kt
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/Operators.kt
@@ -3,8 +3,10 @@ package com.example.kotlinbasic.kotlinprogramms
 fun main() {
     var a=13
     var b=2
-    println(a+b)
-    println(a-b)
-    println(a.toFloat()/b)
-    println(a*b)
+    println(a>b) //true
+    println(a<b) //false
+    println(a>=b) // true
+    println(a==b) // false
+    println(a!=b) //true
+
 }
