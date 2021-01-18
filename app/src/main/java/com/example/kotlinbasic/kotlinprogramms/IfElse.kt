454ea59793059e27d10ea86b6cc68ec312c9dc06
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-01-18 20:43:56 +0530

    If Else

---
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/IfElse.kt
@@ -0,0 +1,38 @@
+package com.example.kotlinbasic.kotlinprogramms
+
+fun main() {
+    var isRaining=true
+   /* if(isRaining){
+        println("Take Umbrella")
+    } else{
+        println("No Rains-Hot day")
+    }
+
+    output :Take Umbrella*/
+
+    //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+/*var x =10
+var y =10
+
+    var result = if(x > y){
+      "X is greater than Y"
+    } else if(x<y){
+        "X is smaller than Y"
+    }
+    else{
+       "X is equal to Y"
+    }
+    println(result)
+
+    output: X is equal to Y*/
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+ /*   var number =20
+    var result = if(number%2==0) "Even Number" else "Odd Number"
+    println(result)
+
+    output:Even Number*/
+
+
+}
