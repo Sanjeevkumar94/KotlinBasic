4a1a35ce9e40f8ce2f9dc7aa2ddd2ed2f1e8cd82
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-01-19 19:00:05 +0530

    Range topic

---
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/Range.kt
@@ -0,0 +1,32 @@
+package com.example.kotlinbasic.kotlinprogramms
+
+fun main(){
+   /* val number=10
+    val result = number in 1..5
+    println(result)
+    output:false*/
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+  /*   val number=5
+    val result = number in 1..5
+    println(result)
+    output:true*/
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+   /* val number =10
+    val result = number in 1..10
+    print(result)
+
+    output:true*/
+
+    //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<,
+
+    /* val number =10
+    val result = number in 1 until 10
+    print(result)
+
+    output:false*/
+
+
+}
