c22cdebc46fb7d57a063ceb904ac7981d53b629d
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-01-19 19:13:26 +0530

    When Topic

---
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/When.kt
@@ -0,0 +1,22 @@
+package com.example.kotlinbasic.kotlinprogramms
+
+fun main() {
+    val animal = "Dog"
+   /* when(animal){
+        "Cat" -> print("Animal is Cat")
+        "Dog" -> print("Animal is Dog")
+        "Tiger" -> print("Animal is Tiger")
+        else->  print("Animal Not Found")
+    }
+    output:Animal is dog*/
+
+    ///when as experssion<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+   /*val result =  when(animal){
+        "Dog" -> "Animal is Dog"
+        "Cat" -> "Animal is Cat"
+        "Tiger" -> "Animal is Tiger"
+        else-> "Animal Not Found"
+    }
+    print(result)
+    output:Animal is Dog*/
+}
