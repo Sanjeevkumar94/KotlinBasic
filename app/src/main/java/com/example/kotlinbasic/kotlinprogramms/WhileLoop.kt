659bb025ded7fa6bcff7cb0c2e7f2de0f15466f1
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-01-20 20:54:34 +0530

    for loop and string templating

--- app/src/main/java/com/example/kotlinbasic/kotlinprogramms/WhileLoop.kt
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/WhileLoop.kt
@@ -2,7 +2,7 @@ package com.example.kotlinbasic.kotlinprogramms
 
 fun main() {
     var i =1
-    while (i<=10){
+   /* while (i<=10){
         println("Hello")
         i++
     }
@@ -10,7 +10,30 @@ fun main() {
     do {
         println("Hello")
         i++
-    } while (i<=10)
+    } while (i<=10)*/
 
+
+   /* for (i in 1..10){
+        println("Hello")
+    }*/
+
+   /* for (i in 1..10 step 2){
+        println("Hello")
+    }*/
+
+   /* for (i in 1 until 5){
+        println("Hello")
+    }
+*/
+
+    /*for (i in 10 downTo 1){
+        println(i)
+    }*/
+
+    var table =2
+
+    for (i in 1..10){
+        println("$table x $i = ${table*i}")
+    }
 
 }
