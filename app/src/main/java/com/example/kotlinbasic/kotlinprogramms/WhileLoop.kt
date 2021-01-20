7d2acdf8dd0c0d5b85aa41ff8111a0db19920fd8
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-01-20 20:35:09 +0530

    while do while

---
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/WhileLoop.kt
@@ -0,0 +1,16 @@
+package com.example.kotlinbasic.kotlinprogramms
+
+fun main() {
+    var i =1
+    while (i<=10){
+        println("Hello")
+        i++
+    }
+
+    do {
+        println("Hello")
+        i++
+    } while (i<=10)
+
+
+}
