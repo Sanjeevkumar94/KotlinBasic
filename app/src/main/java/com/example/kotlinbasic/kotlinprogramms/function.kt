f757885d09247053b1bf0d7eb5d07ffe3576959a
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-01-21 16:33:09 +0530

    fun error

--- app/src/main/java/com/example/kotlinbasic/kotlinprogramms/function.kt
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/function.kt
@@ -2,7 +2,7 @@ package com.example.kotlinbasic.kotlinprogramms
 
 fun main() {
 //    print(add(10,10))
-    message()
+//    message()
 }
 
 /*
@@ -22,10 +22,14 @@ fun add(num1:Int,num2:Int) = num1+num2
 
 //Default fun<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-fun message(count:Int=2){
+/*fun message(count:Int=2){
     for (i in 1..count){
         println("Hello $i")
     }
+}*/
+
+fun add(num1:Int){
+    num1=20    // giving error:val can not be re assigned. parameter of fun are bydefault val.
 }
 
 
