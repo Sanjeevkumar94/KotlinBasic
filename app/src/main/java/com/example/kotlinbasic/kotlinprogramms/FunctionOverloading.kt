47938db5df8dc4b5fd39a3534c8f5c4c2db2e152
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-01-21 19:30:20 +0530

    assign function to variable

--- app/src/main/java/com/example/kotlinbasic/kotlinprogramms/FunctionOverloading.kt
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/FunctionOverloading.kt
@@ -4,8 +4,13 @@ fun main(){
    /* println(add(10,20))
     println(add(10.0,20.0))*/
 
-    //named Argument
-    add(b=10,a=20)
+/*    //named Argument
+    add(b=10,a=20)*/
+
+
+    //assign function to variable
+    var fn =::add
+    println(fn(10,20))
 }
 
 
@@ -16,7 +21,9 @@ fun add(a:Double,b:Double)=a+b*/
 
 //Named Arguments<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-fun add(a:Int,b:Int){
+/*fun add(a:Int,b:Int){
     println("value of a is $a")
     println("value of b is $b")
-}
+}*/
+
+fun add(a:Int,b:Int) = a+b
