78618b4f603dddd515fb850e7d7194565b37cd86
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-01-21 19:27:37 +0530

    named argument

--- app/src/main/java/com/example/kotlinbasic/kotlinprogramms/FunctionOverloading.kt
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/FunctionOverloading.kt
@@ -1,8 +1,22 @@
 package com.example.kotlinbasic.kotlinprogramms
 
 fun main(){
-    println(add(10,20))
-    println(add(10.0,20.0))
+   /* println(add(10,20))
+    println(add(10.0,20.0))*/
+
+    //named Argument
+    add(b=10,a=20)
 }
+
+
+//Function Overloading<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+/*
 fun add(a:Int,b:Int) = a+b
-fun add(a:Double,b:Double)=a+b
+fun add(a:Double,b:Double)=a+b*/
+
+//Named Arguments<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+fun add(a:Int,b:Int){
+    println("value of a is $a")
+    println("value of b is $b")
+}
