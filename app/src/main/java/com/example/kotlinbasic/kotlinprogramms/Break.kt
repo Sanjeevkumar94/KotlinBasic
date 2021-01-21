78fb3c150f826f9c921c14e3ab7203c6a088c568
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-01-21 17:11:37 +0530

    beak continue

---
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/Break.kt
@@ -0,0 +1,33 @@
+package com.example.kotlinbasic.kotlinprogramms
+
+fun main() {
+    // break<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+ /*   for (i in 1..10){
+        if(i==3){
+            break
+        }
+        println(i)
+    }
+    output:1
+            2   */
+
+    //continue<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+   /* for (i in 1..10){
+        if(i==3){
+            continue
+        }
+        println(i)
+    }
+
+    output:1
+    2
+    4
+    5
+    6
+    7
+    8
+    9
+    10*/
+
+}
