7b518b69530a62a02472760a5fc2e86da91007d6
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-01-21 16:25:19 +0530

    one line fun

--- app/src/main/java/com/example/kotlinbasic/kotlinprogramms/function.kt
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/function.kt
@@ -4,6 +4,15 @@ fun main() {
     print(add(10,10))
 }
 
+/*
 fun add(num1:Int,num2:Int):Int{
     return num1+num2
-}
+}*/
+
+/*
+fun add(num1:Int,num2:Int):Int = num1+num2
+*/
+
+/*
+fun add(num1:Int,num2:Int) = num1+num2
+*/
