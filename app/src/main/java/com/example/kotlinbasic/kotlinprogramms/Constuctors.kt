9b41bca0a3e2a6f045dda620e65d4f458a048387
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-01-23 17:46:20 +0530

    primary constructor error

--- app/src/main/java/com/example/kotlinbasic/kotlinprogramms/Constuctors.kt
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/Constuctors.kt
@@ -9,7 +9,14 @@ val ram = Person("Ram Singh")
 
 class Person(val name:String){
     //primary constructor .
-    // if we use var or val then it become member varriable of Class.
-    // if we don't use then it become only parametter variables. for become a class member variable we need to use it on init  block
+    // if we use var or val then it become member variable of Class.
+    // if we don't use then it become only parameters variables. for become a class member variable we need to use it on init  block
+fun show() = name
+}
+
+
+class Person2(name:String){
+    fun show() = name     //giving error because it is only parameter not a class member
+
 
 }
