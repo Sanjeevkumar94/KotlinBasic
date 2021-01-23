ecf65d911c707d59acb635fb3ecc7aec22b862e0
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-01-23 16:25:09 +0530

    arrays

---
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/Arrays.kt
@@ -0,0 +1,84 @@
+package com.example.kotlinbasic.kotlinprogramms
+
+fun main() {
+    /*var arr = arrayOf("one","two","Three")
+    var arr2 = arrayOf<Int>(1,2,3)
+
+    for (i in arr){
+    println(i)
+    }
+
+    for(  (index, elements)in arr.withIndex()){
+        println("$index - $elements")
+    }
+
+    println(arr2[0])
+    println(arr2.get(1))
+    arr2[0]=100
+    println(arr2[0])
+    println(arr.size)
+    arr.set(0,"Hello")
+    println(arr[0])
+    println(arr.count())
+
+    for (i in 0 until arr.size){
+        println(arr[i])
+    }
+*/
+/*
+    var arr = arrayOf("One","two",1,2,3)
+    println(arr[2])
+*/
+
+/*var myArray = Array<Int>(5){0}
+    for(element in myArray){
+        println(element)
+    }
+
+    output:
+    0
+    0
+    0
+    0
+    0*/
+/*var myArray = Array<Int>(5){0}
+
+myArray[0]=15
+    for(element in myArray){
+        println(element)
+    }
+
+    output:
+    15
+    0
+    0
+    0
+    0*/
+
+/*
+    var myArray = intArrayOf(5)
+    print(myArray[0])*/
+
+
+  /*  //It means, you can create an array with null values depending in the size you specified For instance,
+
+    val arrayOfNulls = arrayOfNulls<String>(9)
+    println(arrayOfNulls)
+    */
+
+    /*
+    var a = arrayOf(Student(),Student())
+    a[0].a=10
+    a[1].a=20
+
+    for( item in a ){
+        println(item.a)
+    }*/
+
+}
+
+
+class  Student {
+    var a=0
+    var b =9
+}
