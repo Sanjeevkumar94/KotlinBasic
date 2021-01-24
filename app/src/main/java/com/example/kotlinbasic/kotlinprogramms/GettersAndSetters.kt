7cc15b38abd3ca1f84b8c5c807ec06667a0d04ba
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-01-24 16:01:34 +0530

    setter

--- app/src/main/java/com/example/kotlinbasic/kotlinprogramms/GettersAndSetters.kt
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/GettersAndSetters.kt
@@ -2,15 +2,33 @@ package com.example.kotlinbasic.kotlinprogramms
 
 fun main() {
 
-  var student2 = Student2("sanj kumar")
+  var student2 = Student2("sanj kumar",23)
     println(student2.userName)
+
+    student2.userMarks = 23
 }
 
 
-class Student2(name:String){
+class Student2(name:String,marks:Int){
     var userName:String=name
-    get() {
-        return field.toUpperCase()
+        get() {
+            return field.toUpperCase()
+        }
+    var userMarks:Int=marks
+    set(value) {
+        if(value>33){
+            println("You are pass")
+        } else{
+            println("You are fail")
+
+        }
     }
 
+
+    //by dafault getter and setter
+    var email:String =""
+    get() = field
+    set(value){
+        field = value
+    }
 }
