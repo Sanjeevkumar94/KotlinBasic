ab3935b3e2c60ee8cd7f04ad794200b1066bf3ec
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-01-30 17:09:24 +0530

    constructor error

--- app/src/main/java/com/example/kotlinbasic/kotlinprogramms/OverloadingInheritance.kt
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/OverloadingInheritance.kt
@@ -3,7 +3,7 @@ package com.example.kotlinbasic.kotlinprogramms
 fun main() {
 
 
-    var a = OverloadingInheritance()
+  /*  var a = OverloadingInheritance()
     a.add(1,2)
 
     var b = OverLoading2()
@@ -11,9 +11,9 @@ fun main() {
     b.add(4.0,5.0)
 
     var b2:OverloadingInheritance = OverLoading2()
-    b2.add(6,7)
+    b2.add(6,7)*/
 
-
+    var c = ConstructorOverLoading("Arjun")
 }
 
 
@@ -23,4 +23,18 @@ open class OverloadingInheritance{
 
 class OverLoading2:OverloadingInheritance() {
      fun add(a: Double, b: Double) =a.plus(b)
+}
+
+
+class ConstructorOverLoading(var name:String="",var fname:String ="",var age:Int= 0){
+    constructor(lname:String):this("dk","whw",45){
+        println("Helooo")
+    }
+
+
+    constructor(raavan:Int){
+        println("dfggdfhfghfghgfhgf")
+    }
+
+    //giving error primary constructor call expected because every secondary constructor need call to primary constructor with defined.
 }
