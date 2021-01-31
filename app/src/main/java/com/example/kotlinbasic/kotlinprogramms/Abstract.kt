0318dce2efb4937381d738fc3a85a53643e0fa80
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-01-31 20:03:13 +0530

    Doc of abstract class

--- app/src/main/java/com/example/kotlinbasic/kotlinprogramms/Abstract.kt
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/Abstract.kt
@@ -33,3 +33,24 @@ class AbstractEx2:AbstractEx() {
     override var a: String = "Hello"
 
 }
+
+
+
+/*
+doc
+
+A class which is declared with abstract keyword is known as abstract class.
+An abstract class cannot be instantiated. Means, we cannot create object of abstract class.
+The method and properties of abstract class are non-abstract unless they are explicitly declared as abstract.
+Declaration of abstract class
+abstract class A {
+    var x = 0
+    abstract fun doSomething()
+}
+
+Abstract classes are partially defined classes, methods and properties which are no implementation but must be implemented into derived class.
+If the derived class does not implement the properties of base class then is also meant to be an abstract class.
+
+Abstract class or abstract function does not need to annotate with open keyword as they are open by default.
+Abstract member function does not contain its body.
+The member function cannot be declared as abstract if it contains in body in abstract class.*/
