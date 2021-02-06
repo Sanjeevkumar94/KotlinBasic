1badbb6fecc4bae4ca354775f6a8ecd25d233813
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-02-06 18:31:48 +0530

    singleton ex

--- app/src/main/java/com/example/kotlinbasic/kotlinprogramms/ObjectAndSingleton.kt
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/ObjectAndSingleton.kt
@@ -2,8 +2,15 @@ package com.example.kotlinbasic.kotlinprogramms
 
 
 fun main() {
-    println(ObjectDemo.cityName)
-    println(ObjectDemo2.cityName())
+    /*println(ObjectDemo.cityName)
+    println(ObjectDemo2.cityName())*/
+
+    SharingWidget.likesTwitter()
+    SharingWidget.likesTwitter()
+    SharingWidget.fbLikes()
+
+    println(SharingWidget.displayTotalLikes())
+
 }
 
 
@@ -13,4 +20,15 @@ object ObjectDemo {
 
 object ObjectDemo2 {
     fun cityName() = "Sirhind"
+}
+
+object SharingWidget {
+    private var twitteLikes =0
+    private var fbLikes =0
+
+    fun likesTwitter() = twitteLikes++
+    fun fbLikes() = fbLikes++
+
+    fun displayTotalLikes() = "Twitter Likes -- $twitteLikes  Fb Likes -- $fbLikes"
+
 }
