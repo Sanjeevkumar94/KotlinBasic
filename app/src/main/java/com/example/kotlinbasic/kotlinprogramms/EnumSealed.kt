0a66bd0ecf3c3ea556178b134f4b079f54a95111
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-02-10 12:41:49 +0530

    enum ex

--- app/src/main/java/com/example/kotlinbasic/kotlinprogramms/EnumSealed.kt
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/EnumSealed.kt
@@ -4,15 +4,27 @@ fun main() {
 
     var day = Day.SATURDAY
     println(Day.FRIDAY)
+    println(day.number)
+    println(day.printDay())
+    println(Day.MONDAY.printDay())
+
+    for(i in Day.values()){
+        println(i)
+        println(i.number)
+    }
 }
 
 
-enum class Day{
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY
+enum class Day(var number:Int){
+    MONDAY(1),            //monday,tuesday all are objects.
+    TUESDAY(2),
+    WEDNESDAY(3),
+    THURSDAY(4),
+    FRIDAY(5),
+    SATURDAY(6),
+    SUNDAY(7);
+
+    fun printDay(){
+        println("Day is $this")
+    }
 }
