f2a019420e6825e820631b94de34328443351de8
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-02-10 12:23:29 +0530

    basic enum

---
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/EnumSealed.kt
@@ -0,0 +1,18 @@
+package com.example.kotlinbasic.kotlinprogramms
+
+fun main() {
+
+    var day = Day.SATURDAY
+    println(Day.FRIDAY)
+}
+
+
+enum class Day{
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
