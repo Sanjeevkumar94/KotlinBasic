44bc44971fe5d284b5903f5c197920dc37152858
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-02-10 13:02:47 +0530

    enum doc

--- app/src/main/java/com/example/kotlinbasic/kotlinprogramms/EnumSealed.kt
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/EnumSealed.kt
@@ -28,3 +28,17 @@ enum class Day(var number:Int){
         println("Day is $this")
     }
 }
+
+
+
+//doc
+/*
+In programming, sometimes there arises a need for a type to have only certain values. To accomplish this, the concept of enumeration was introduced. Enumeration is a named list of constants.
+In Kotlin, like many other programming languages, an enum has its own specialized type, indicating that something has a number of possible values. Unlike Java enums, Kotlin enums are classes.
+
+Some important points about enum classes in kotlin –
+
+Enum constants aren’t just mere collections of constants- these have properties, methods etc
+Each of the enum constants acts as separate instances of the class and separated by commas.
+Enums increases readability of your code by assigning pre-defined names to constants.
+An instance of enum class cannot be created using constructors.*/
