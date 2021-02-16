637b19c7ef44da261e18300c24af63e231596c39
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-02-16 12:09:48 +0530

    lambda

---
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/LambdaExperssion.kt
@@ -0,0 +1,42 @@
+package com.example.kotlinbasic.kotlinprogramms
+
+fun main() {
+/*    var lambda1 = {x:Int,y:Int->x+y}
+    println(lambda1(10,20))*/
+
+/*    var multiLinedLambda = {
+        println("Hello")
+        2 //last line is return type
+    }
+    println(multiLinedLambda())*/
+
+/*    var singleParamLambda ={x:Int->x*x}
+    println(singleParamLambda(2))*/
+
+
+/*    var singleParamLambdaWithDefineType:(Int)->Int = {x->x*x}
+    println(singleParamLambdaWithDefineType(2))*/
+
+ /*   var simplySingleParamLambda:(Int)->Int = {it*it}
+    println(simplySingleParamLambda(2))*/
+
+
+    /*  val sayHi = {msg:String-> println(msg)}
+      sayHi("Hi Sanj")*/
+
+ /*   val sayHi2:(String)->Unit = {msg-> println(msg)}
+    sayHi2("Hi Sanj")
+*/
+
+    calculator(10,20,{x,y->x+y}) // with lambda
+    calculator(20,40,::sum2) // withFunction Type
+    val fn: (a: Int, b: Int) -> Int = ::sum2 // assign function in variable
+    calculator(20,40,fn) // withFunction Type
+}
+fun sum2 (a:Int,b:Int):Int{
+    return a+b
+}
+
+fun calculator(a:Int,b:Int,fn:(Int,Int)->Int){
+    println(fn(a,b))
+}
