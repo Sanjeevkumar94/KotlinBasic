b90e62fc8bee2d5c310321030351b09da9ac7576
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-02-19 20:56:39 +0530

    Filter Map ForEarch

---
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/FilerMapDemo.kt
@@ -0,0 +1,46 @@
+package com.example.kotlinbasic.kotlinprogramms
+
+fun main() {
+    val nums = listOf<Int>(1,2,3,4,5)
+   /* println(isOdd(3))
+    println(isOdd(2))*/
+
+   /* val list = nums.filter(::isOdd)
+    println(list)       //[1, 3, 5]
+
+    val list2 = nums.filter({a:Int->a%2!=0})
+    val list3 = nums.filter {a:Int->a%2!=0}
+    val list4 = nums.filter {a:Int->a%2!=0}
+    val list5 = nums.filter{it%2!=0}
+    println(list2)       //[1, 3, 5]
+    println(list3)       //[1, 3, 5]
+    println(list4)       //[1, 3, 5]
+    println(list5)       //[1, 3, 5]*/
+
+
+
+    val userList = listOf<User>(
+        User(1,"Sanj"),
+        User(1,"Lucky"),
+        User(3,"Mohan")
+    )
+
+ /*   val userList2 = userList.filter { it.id ==1 }
+    println(userList2)
+*/
+
+    val paidUser = userList.map {
+        PaidUser(it.id,it.name,"Admin")
+        PaidUser(it.id,it.name,"Normal User")
+    }
+
+    println(paidUser)
+    paidUser.forEach{
+        println(it.name)
+    }
+
+}
+
+fun isOdd(a:Int) = a%2!=0
+data class User(val id:Int,val name:String)
+data class PaidUser(val id:Int,val name:String,val type:String)
