8ce3406bddbc22cf36e769580acfd10010f24cca
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-02-20 19:17:04 +0530

    inline

--- app/src/main/java/com/example/kotlinbasic/kotlinprogramms/inLineDemo.kt
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/inLineDemo.kt
@@ -2,19 +2,34 @@ package com.example.kotlinbasic.kotlinprogramms
 
 fun main() {
 
-    calculateTimeAndRun{ loop(100,200)}
+    calculateTimeAndRun{
+        loop(1000)}
 }
 
-fun calculateTimeAndRun(fn:(Int)->Unit){
+inline fun calculateTimeAndRun(fn:()->Unit){   // is ke liye class banti hai fir instance banata hai uske baad use kr pate hai
 
+    val start = System.currentTimeMillis();
+    fn()
+    val end = System.currentTimeMillis()
+    println("Time taken ${end - start} ms")
 }
 
-
-fun asd(){
-
-}
-fun loop(n:Long,a:Int){
+fun loop(n:Long){
     for (i in 1..n){
         //
     }
 }
+
+
+/*
+//doc
+Inline Function
+An inline function is declare with a keyword inline. The use of inline function enhances the performance of higher order function. The inline function tells the compiler to copy parameters and functions to the call site.
+
+The virtual function or local function cannot be declared as inline. Following are some expressions and declarations which are not supported anywhere inside the inline functions:
+
+Declaration of local classes
+Declaration of inner nested classes
+Function expressions
+Declarations of local function
+Default value for optional parameters*/
