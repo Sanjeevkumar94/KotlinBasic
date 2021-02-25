e3a5dbe4c41e90247892440baf17b6db46edfbad
Sanjeevkumar94 <34913771+Sanjeevkumar94@users.noreply.github.com>
2021-02-25 20:18:14 +0530

    vararg

--- app/src/main/java/com/example/kotlinbasic/kotlinprogramms/varargs.kt
+++ app/src/main/java/com/example/kotlinbasic/kotlinprogramms/varargs.kt
@@ -1,2 +1,12 @@
 package com.example.kotlinbasic.kotlinprogramms
 
+fun main() {
+adds(10,20,30)
+}
+
+
+fun adds(vararg values:Int){
+    for (elements in values){
+        println(elements)
+    }
+}
